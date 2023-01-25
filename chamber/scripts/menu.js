ff559ed4d57f4230e354f9eaef70f428cbd58a99
mck06016 <mck06016@buyi.edu>
2023-01-25 07:57:06 -0800

    chamber site initial work

---
+++ chamber/scripts/menu.js
@@ -0,0 +1,7 @@
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
--- lesson3/scripts/lastmodified.js
+++ lesson3/scripts/lastmodified.js
@@ -1,13 +1,5 @@
-//const lastUpdated = document.querySelector("#lastUpdated");
-
-//lastUpdated.innerHTML = (
-// <span class="Highlight">'Last Updated: ${document.lastModified}'</span>
-//);
-
-//I have been coding in circles for hours. I don't know how to get the code to appear correctly on the page dynamically using JavaScript. I am taking 121 Concurently and don't have any reference for how to make this happen other then the codePen activity we completed this week. - - I finally resorted to Copy Paste of the Code Pen - Then edited to look like what we needed for the Assignment Portal - - This is not original to me.//
-
 const date1 = document.querySelector("#date1");
 
 try {
   date1.innerHTML = `Last Updated: <span class="highlight">${document.lastModified}</span>`;
-} catch (e) {} //I Dont know what catch (e) does //
+} catch (e) {} 
