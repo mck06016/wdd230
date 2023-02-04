42ee0216f04b64b80806ae8ad5c25d8f74ea9717
mck06016 <mck06016@buyi.edu>
2023-02-04 14:09:30 -0800

    Corrected Week 04

--- chamber/scripts/todaysdate.js
+++ chamber/scripts/todaysdate.js
@@ -1,6 +1,6 @@
 
 //document.querySelector('date').innerHTML = currentDate.getDate();
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-var today = new Date().toLocaleDateString("en-US",options);
+var today = new Date().toLocaleDateString("en-GB",options);
 
 document.getElementById("date").innerHTML = today;
