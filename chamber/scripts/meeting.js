e0309624725875dfcf8ded80352c5aa725a5aace
mck06016 <mck06016@buyi.edu>
2023-02-04 18:58:00 -0800

    Commerce Site Week 05

---
+++ chamber/scripts/meeting.js
@@ -0,0 +1,36 @@
+const meeting = document.getElementById("banner-content");
+
+const currentDate = new Date();
+
+const dayOfWeek = currentDate.getDay();
+console.log("Day #: ", dayOfWeek)
+
+let alertMeeting; 
+
+switch (dayOfWeek){
+  case 0:
+    alertMeeting = "";
+    break;
+  case 1: 
+    alertMeeting = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    break;
+  case 2: 
+    alertMeeting = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+    break;
+  case 3:
+    alertMeeting = "";
+    break;
+  case 4:
+    alertMeeting = "";
+    break;
+  case 5:
+    alertMeeting = "";
+    break;
+  case 6:
+    alertMeeting = "";
+    break;
+}
+meeting.innerHTML = alertMeeting;
+console.log(alertMeeting);
+
+//"🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
