443cd9de585920e2976049092067c7b18461c472
mck06016 <mck06016@byui.edu>
2023-09-13 08:05:32 -0700

    moving JS to one file

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,5 @@
+//Copywrite - year
+
 const copyYear = document.querySelector("#year");
 
 try {
@@ -8,6 +10,16 @@ try {
     "en-Us",
     options
   )}`;
+} catch (e) {
+  console.log("Error with code or your browser does not support Locale");
+}
+
+//Date Last Modified
+
+const date1 = document.querySelector("#date1");
+
+try {
+  date1.innerHTML = `<span class="highlight">${document.lastModified}</span>`;
 } catch (e) {
   console.log("Error with code or your browser does not support Locale");
 }
